58807e75303a669ea9a27101314e4a44f57e227e
Emre Özbey <emre33ozbey.com@gmail.com>
2015-02-03 10:53:59 +0200

    uygulama1 eklendi

---
+++ uygulama1.rb
@@ -0,0 +1,19 @@
+def topla sayi1, sayi2
+	sayi1.to_f+sayi2
+end
+puts topla 43, 577
+
+def cikar sayi1, sayi2
+	sayi1.to_f-sayi2
+end
+puts cikar 320,533
+
+def carp sayi1, sayi2
+	sayi1.to_f*sayi2
+end
+puts carp 15,15
+
+def bolme sayi1, sayi2
+	sayi1.to_f*sayi2
+end
+puts bolme 78,10
